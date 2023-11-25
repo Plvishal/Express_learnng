19e0f40726db8f69fcaf981547fd0a70b3841d6d
Vishal <vishalpal1501@gmail.com>
2023-11-25 12:29:17 +0530

    server creating using via a http

--- 1_getting_Started/server.js
+++ 1_getting_Started/server.js
@@ -1,1 +1,8 @@
-let a = 'b';
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('Welcome');
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
