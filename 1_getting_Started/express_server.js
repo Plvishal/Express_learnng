26875b51c4bac478aee9d6e085dc7a6cef774b53
Vishal <vishalpal1501@gmail.com>
2023-11-25 14:54:43 +0530

    Global Level middleware

--- 1_getting_Started/express_server.js
+++ 1_getting_Started/express_server.js
@@ -13,6 +13,13 @@ function secondMiddlware(req, res, next) {
   console.log('Second Middleware');
   next();
 }
+function globalLevel(req, res, next) {
+  console.log('Global level middleware');
+  next();
+}
+// This is a global level  middle ware
+server.use(globalLevel);
+// this is a root level middleware here root is :/send
 server.get('/', [firstMidlleware, secondMiddlware], (req, res) => {
   res.send('hello');
 });
