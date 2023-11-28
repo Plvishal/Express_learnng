cfdfcc6fc4bd099227037c0be84911aa497471c5
Vishal <vishalpal1501@gmail.com>
2023-11-28 21:35:31 +0530

    JWT Authentication-1

--- Getting_Started_with_API/src/features/user/user.controller.js
+++ Getting_Started_with_API/src/features/user/user.controller.js
@@ -1,4 +1,5 @@
 import UserModel from './user.model.js';
+import jwt from 'jsonwebtoken';
 export default class UserController {
   signUp(req, res) {
     const { name, email, password, type } = req.body;
@@ -10,7 +11,15 @@ export default class UserController {
     if (!result) {
       return res.status(400).send('Incorrect Credentials');
     } else {
-      return res.status(200).send('Login Successful');
+      // 1.create token
+      const token = jwt.sign(
+        { userId: result.id, email: result.email },
+        'jqpB0H3gxJy20LmUX2AKD3YTPTj3hd9G',
+        { expiresIn: '1h' }
+      );
+      // 2.Send token
+
+      return res.status(200).send(token);
     }
   }
 }
